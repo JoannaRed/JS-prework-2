5b4b146fb15560d1282c9cd7c00f3c26d273aa5c
Joanna Baranowska <j.baranowska@hotmail.com>
2020-07-06 10:27:17 +0200

    Add computers option

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+else(randomNumber == 2){
+  computerMove = 'papier';
+}
+else if(randomNumber == 3){
+  computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
