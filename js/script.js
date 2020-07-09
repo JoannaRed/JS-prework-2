1f33d01c9656b8d473b4e4c227dcbe8c542cf202
Joanna Baranowska <j.baranowska@hotmail.com>
2020-07-09 22:49:25 +0200

    Add buttons text

--- js/script.js
+++ js/script.js
@@ -1,5 +1,79 @@
-function playGame(playerInput){
+function playGame(playerMove){
   clearMessages
+
+    
+  function displayResult(argComputerMove, argPlayerMove) {
+    printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+  } 
+
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+  console.log('Wylosowana liczba to: ' + randomNumber);
+
+  let computerMove = getMoveName(randomNumber);
+
+  printMessage('Ruch komputera to: ' + computerMove);
+
+  //let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+  //console.log('Gracz wpisał: ' + playerInput);
+
+  //let playerMove = getMoveName(playerInput);
+
+  printMessage('Twój ruch to: ' + playerMove);
+
+  if (computerMove == 'kamień' && playerMove == 'papier') {
+    printMessage('Ty wygrywasz!');
+  }
+  else if (computerMove == 'kamień' && playerMove == 'nożyce') {
+    printMessage('Przegrywasz!');
+  }
+  else if (computerMove == 'kamień' && playerMove == 'kamień') {
+    printMessage('Remis!');
+  }
+  else if (computerMove == 'papier' && playerMove == 'papier') {
+    printMessage('Remis!');
+  }
+  else if (computerMove == 'papier' && playerMove == 'nożyce') {
+    printMessage('Ty wygrywasz!');
+  }
+  else if (computerMove == 'papier' && playerMove == 'kamień') {
+    printMessage('Przegrywasz!');
+  }
+  else if (computerMove == 'nożyce' && playerMove == 'papier') {
+    printMessage('Przegrywasz!');
+  }
+  else if (computerMove == 'nożyce' && playerMove == 'nożyce') {
+    printMessage('Remis!');
+  }
+  else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+    printMessage('Ty wygrywasz!');
+  }
+
+}
+function buttonClicked(){
+  printMessage('Zagrales kamien');
+}
+
+let playRock = document.getElementById('play-rock');
+
+playRock.addEventListener('click', buttonClicked);
+
+function buttonClicked(){
+  printMessage('Zagrales papier');
+}
+
+let playPaper = document.getElementById('play-paper');
+
+playPaper.addEventListener('click', buttonClicked);
+
+function buttonClicked(){
+  printMessage('Zagrales nozyczki');
+}
+
+let playScissors = document.getElementById('play-scissors');
+
+playScissors.addEventListener('click', buttonClicked);
+
 
 function getMoveName(argMoveId) {
   if (argMoveId == 1){
@@ -13,62 +87,6 @@ function getMoveName(argMoveId) {
   printMessage('Nie znam ruchu o id ' + argMoveId + '.');
   return 'nieznany ruch';
 }
-
-function displayResult(argComputerMove, argPlayerMove) {
-  printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
-}
-
-let randomNumber = Math.floor(Math.random() * 3 + 1);
-
-console.log('Wylosowana liczba to: ' + randomNumber);
-
-let computerMove = getMoveName(randomNumber);
-
-printMessage('Ruch komputera to: ' + computerMove);
-
-//let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-//console.log('Gracz wpisał: ' + playerInput);
-
-//let playerMove = getMoveName(playerInput);
-
-printMessage('Twój ruch to: ' + playerMove);
-
-if (computerMove == 'kamień' && playerMove == 'papier') {
-  printMessage('Ty wygrywasz!');
-}
-else if (computerMove == 'kamień' && playerMove == 'nożyce') {
-  printMessage('Przegrywasz!');
-}
-else if (computerMove == 'kamień' && playerMove == 'kamień') {
-  printMessage('Remis!');
-}
-else if (computerMove == 'papier' && playerMove == 'papier') {
-  printMessage('Remis!');
-}
-else if (computerMove == 'papier' && playerMove == 'nożyce') {
-  printMessage('Ty wygrywasz!');
-}
-else if (computerMove == 'papier' && playerMove == 'kamień') {
-  printMessage('Przegrywasz!');
-}
-else if (computerMove == 'nożyce' && playerMove == 'papier') {
-  printMessage('Przegrywasz!');
-}
-else if (computerMove == 'nożyce' && playerMove == 'nożyce') {
-  printMessage('Remis!');
-}
-else if (computerMove == 'nożyce' && playerMove == 'kamień') {
-  printMessage('Ty wygrywasz!');
-}
-
-}
-function buttonClicked(){
-  printMessage('Guzik został kliknięty');
-}
-
-let 'playRock' = document.getElementById('play-rock');
-
-playRock.addEventListener('click', buttonClicked);
 
 //playGame(3);
 
